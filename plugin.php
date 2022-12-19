80c9624ba49e0e3b497735d7099df281974a25aa
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2022-12-19 11:30:02 +0100

    Init.

---
+++ plugin.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Pronamic Deployer Test Plugin
+ *
+ * @package           PronamicDeployerTestPlugin
+ * @author            Pronamic
+ * @copyright         2022 Pronamic
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Pronamic Deployer Test Plugin
+ * Plugin URI:        https://github.com/pronamic/wp-deployer-test-plugin
+ * Description:       Pronamic Deployer test plugin.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Pronamic
+ * Author URI:        https://www.pronamic.eu/
+ * Text Domain:       pronamic-deployer-test-plugin
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Update URI:        https://www.pronamic.eu/plugins/pronamic-deployer-test-plugin/
+ */
---
+++ readme.txt
@@ -0,0 +1,11 @@
+=== Pronamic Deployer Test Plugin ===
+Contributors: pronamic
+Donate link: https://www.pronamic.eu/
+Tags: pronamic, deployer, test, plugin
+Requires at least: 4.7
+Tested up to: 5.4
+Stable tag: 1.0.0
+Requires PHP: 7.0
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Pronamic Deployer test plugin.
